59e5d53b82f799910533a4613a6e2cfd6a37b03d
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-06-30 21:19:30 +0530

    remove button

--- src/App.jsx
+++ src/App.jsx
@@ -25,34 +25,35 @@ function App() {
             <div className="todo">
               <div className="left">
                 <input onChange={(e)=>{
-                  console.log(object); //object may object, contain text and status 
+                  // console.log(object); //object may object, contain text and status 
                   // Change todo status
                   setToDo(toDos.filter(eachObjects=>{
                     // For filter, return the needed objects
                     if(eachObjects.id===object.id){
                       eachObjects.status=e.target.checked;
-                      console.log(eachObjects.status); //may be boolean
+                      // console.log(eachObjects.status); //may be boolean
                     }
                     return eachObjects
                   }))
+                // For accessing text from Todos 
                 }} value={object.status} type="checkbox" name="" id="" />
-                {/* accessing text from Todos */}
-                <p>{object.text}</p>
                 {/* Display Todo */}
+                <p>{object.text}</p>
+                
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
-                {/* Remove button */}
+              {/* Remove button */}
+              <i onClick={() => setToDos(toDos.filter(eachObjects => eachObjects.id !== object.id))} className="fas fa-times"></i>
               </div>
             </div>
           )
         })}
-        {toDos.map((object)=>{
+        {/* {toDos.map((object)=>{
           if(object.status){
             return (<h1>{object.text}</h1>)
           }
           return null
-        })}
+        })} */}
         
       </div>
     </div>
